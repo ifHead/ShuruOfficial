3190e0821d03435af1d06daaeff9ca57bea92af1
Sanhead <ijn7uhb@gmail.com>
2023-01-24 23:54:29 +0900

    Joint

--- js/gsap/pin.js
+++ js/gsap/pin.js
@@ -1,44 +1,93 @@
 gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
 
 let smoother = ScrollSmoother.create({
-    smooth: 1,
-    effects: true
+    content: "#smooth_content",
+    smooth: 2,
 });
 
-// gsap.registerPlugin(ScrollTrigger);
+let tl = gsap.timeline();
 
-// const tl = gsap.timeline();
+window.addEventListener("resize", () => {
+    if (window.RT) clearTimeout(window.RT);
+    window.RT = setTimeout(function () {
+        this.location.reload(false); /* false to get page from cache */
+    }, 200);
+});
 
-// tl.to("#StructureContainer", {
+
+tl
+.to("#Structure_1", {
+    scrollTrigger: {
+        trigger: "#StructureTrigger_1",
+        start: "top+=" + window.innerWidth * 0.3 + " center",
+        end: "bottom+=" + window.innerWidth + " top",
+        pin: "#StructureContainer",
+        scrub: true,
+        markers: true,
+    },
+})
+.to("#Structure_2", {
+    scrollTrigger: {
+        trigger: "#StructureTrigger_2",
+        start: "top center",
+        end: "bottom top",
+        // pin: "#StructureContainer",
+        scrub: true,
+        markers: true,
+    },
+    opacity: 1
+})
+.to("#Structure_3", {
+    scrollTrigger: {
+        trigger: "#StructureTrigger_3",
+        start: "top+=" + window.innerWidth * 0.3 + " center",
+        end: "bottom+=" + window.innerWidth * 0.3 + " top",
+        scrub: true,
+        markers: true,
+    },
+    opacity: 1
+})
+.to("#CorpDemo", {
+    scrollTrigger: {
+        trigger: "#StructureTrigger_1",
+        start: "top+=" + window.innerWidth * 0.3 + " center ",
+        end: "bottom+=" + window.innerWidth + " top",
+        scrub: true
+    },
+    y: -window.innerWidth * 1.3,
+    duration: 10
+})
+
+
+
+// .to("#Structure_3", {
 //     scrollTrigger: {
-//         trigger: "#StructureContainer",
-//         start: "bottom top",
-//         end: "",
-//         pin: true,
+//         trigger: "#StructureTrigger_3",
+//         start: "top+=" + window.innerHeight / 4 + " center",
+//         pin: "#StructureContainer",
 //         scrub: true,
 //         markers: true,
-//     }
-// });
-// // .to("#Structure_2", {opacity: 0})
-// // .to("#Structure_3", {opacity: 0});
+//     },
+//     opacity: 1
+// })
 
-// // ScrollTrigger.create({
-// //     animation: tl,
-// //     trigger: "#StructureContainer",
-// //     start: "top top",
-// //     markers: true,
-// //     scrub: true,
-// //     pin: true,
-// // });
+// // // ScrollTrigger.create({
+// // //     animation: tl,
+// // //     trigger: "#StructureContainer",
+// // //     start: "top top",
+// // //     markers: true,
+// // //     scrub: true,
+// // //     pin: true,
+// // // });
 
-// // let scrollTopVal = 0;
+// // // let scrollTopVal = 0;
 
-// // window.addEventListener('scroll', function (e) {
-// //     scrollTopVal = window.scrollY;
-// //     bodyScrollBar.scrollTop = window.scrollY;
-// // }
+// // // window.addEventListener('scroll', function (e) {
+// // //     scrollTopVal = window.scrollY;
+// // //     bodyScrollBar.scrollTop = window.scrollY;
+// // // }
 
-// // ScrollTrigger.scrollerProxy(document.body, {
-// // })
+// // // ScrollTrigger.scrollerProxy(document.body, {
+// // // })
 
-// // bodyScrollBar.addListener(ScrollTrigger.update);
+// // // bodyScrollBar.addListener(ScrollTrigger.update);
