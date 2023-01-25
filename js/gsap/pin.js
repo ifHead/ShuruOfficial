051dd0515ad15bacbda93f5c8f15010b525302d3
Sanhead <ijn7uhb@gmail.com>
2023-01-25 10:23:04 +0900

    c

--- js/gsap/pin.js
+++ js/gsap/pin.js
@@ -2,7 +2,7 @@ gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
 
 let smoother = ScrollSmoother.create({
     content: "#smooth_content",
-    smooth: 2,
+    smooth: 1.3,
 });
 
 let tl = gsap.timeline();
@@ -13,7 +13,6 @@ window.addEventListener("resize", () => {
         this.location.reload(false); /* false to get page from cache */
     }, 200);
 });
-
 
 tl
 .to("#Structure_1", {
@@ -29,8 +28,8 @@ tl
 .to("#Structure_2", {
     scrollTrigger: {
         trigger: "#StructureTrigger_2",
-        start: "top center",
-        end: "bottom top",
+        start: "top-=" + window.innerWidth * 0.3 + "center",
+        end: "top top",
         // pin: "#StructureContainer",
         scrub: true,
         markers: true,
@@ -40,7 +39,7 @@ tl
 .to("#Structure_3", {
     scrollTrigger: {
         trigger: "#StructureTrigger_3",
-        start: "top+=" + window.innerWidth * 0.3 + " center",
+        start: "top-=" + window.innerWidth * 0.3 + " center",
         end: "bottom-=" + window.innerWidth * 0.3 + " top",
         scrub: true,
         markers: true,
@@ -52,13 +51,80 @@ tl
         trigger: "#StructureTrigger_1",
         start: "top+=" + window.innerWidth * 0.3 + " center ",
         end: "bottom+=" + window.innerWidth + " top",
-        scrub: true
+        scrub: true,
     },
-    y: -window.innerWidth * 1.3,
-    duration: 10
+    y: window.innerWidth / innerHeight > 1 ? -window.innerWidth * 0.6 : -window.innerWidth * 1.2,
+})
+.from(".TitleBox1", {
+    scrollTrigger: {
+        trigger: "#StructureTrigger_1",
+        start: "top+=" + window.innerHeight * 0.1 + " bottom",
+        end: "center center+=" + window.innerHeight * 0.15,
+        scrub: true,
+    },
+    opacity: -1.5,
+    y: -window.innerHeight * 2,
+    x: -window.innerWidth * 0.7
+})
+.from(".TitleBox2", {
+    scrollTrigger: {
+        trigger: "#StructureTrigger_1",
+        start: "top+="+window.innerHeight*0.2 +" bottom",
+        end: "center center",
+        scrub: true,
+    },
+    opacity: -1.5,
+    y: -window.innerHeight * 2,
+    x: -window.innerWidth
+})
+.from(".TitleBox3", {
+    scrollTrigger: {
+        trigger: "#StructureTrigger_1",
+        start: "top+=" + window.innerHeight * 0.3 + " bottom",
+        end: "center center-=" + window.innerHeight * 0.15,
+        scrub: true,
+    },
+    opacity: -1.5,
+    y: -window.innerHeight,
+    x: -window.innerWidth
 })
 
+let tlse = gsap.timeline();
 
+tlse
+.to("#CloudLineContainer", {
+    scrollTrigger: {
+        trigger: "#CloudLineContainer",
+        start: "top bottom",
+        scrub: true
+    },
+    y: -window.innerHeight * 2.2
+})
+.from(".BigTitleBox", {
+    scrollTrigger: {
+        trigger: ".BigTitleBox",
+        start: "top bottom-=" + window.innerHeight * 0.1,
+        scrub: true
+    },
+    y: -300,
+    opacity: 0.5,
+    duration: 2
+});
+
+gsap.to("#Band", {
+    scrollTrigger: {
+        trigger: "#Band",
+        start: "top bottom",
+        toggleActions: 'restart none none none',
+        once: false,
+        markers: true
+    },
+    x: 0,
+    duration: 120
+})
+// .from("ShuPush"){
+
+// }
 
 // .to("#Structure_3", {
 //     scrollTrigger: {
